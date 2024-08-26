dd5f09586385279091b315ba0b1b5cef55e59c71
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2024-08-26 08:19:05 +0100

    Update BinaryDecimalConverter.py

--- BinaryDecimalConverter.py
+++ BinaryDecimalConverter.py
@@ -1,13 +1,11 @@
-
 def is_binary(s):
-    return all(char in '01' for char in s) # Checking if the string s is a valid binary number
+    return all(char in '01' for char in s) # Checking if the string "s" is a valid binary number
 
 def binary_to_decimal(binary_str):
-    return int(binary_str, 2) # Converting binary string to decimal number
+    return int(binary_str, 2) # Converting a binary string to a decimal number
 
 def decimal_to_binary(decimal_number):
     return bin(decimal_number).replace("Ob", "") # converts a decimal number to its binary string representation and then removes the '0b' prefix that Python includes by default.
-
 
 
 user_choice = input("Type 'd-b' if you would like decimal number to be converted into binary number and type 'b-d' if you would like binary number to be converted into decimal number: ")
