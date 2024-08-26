e8db8959193e258785e297ae523aa57f33ee5c53
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2024-08-26 08:30:23 +0100

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,5 @@
 def selection_sort(arr):
-    for i in range(0, len(arr) - 1):  # the last index can be skipped because when algorithm arrives at this point the last element is in the correct position
+    for i in range(0, len(arr) - 1):  # the last index can be skipped because when algorithm arrives at this point the last element is already in the correct position
         cur_min_index = i
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[cur_min_index]:
